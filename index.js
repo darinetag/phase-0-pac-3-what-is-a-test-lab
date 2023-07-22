239921508845b23ef4a5fcc5bae7c0b001545501
darinetag <tagdarinefatma@gmail.com>
2023-07-22 21:54:41 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const name = "Joe";
-const height = 74;
+const name = "Susan";
+const height = 20; // Updated the height value to 20
 const message = `${name} is ${height} inches tall`;
 
-module.exports = {name, height, message};
+module.exports = { name, height, message };
+
+
